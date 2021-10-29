a94d5f910bc3c8952a3e3949e86119b0022fe7c5
Cherry708 <aluser8241@ieselcaminas.org>
2021-10-29 17:50:43 +0200

    Practica 5

---
+++ src/practica5/Reducciones.java
@@ -0,0 +1,162 @@
+package practica5;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Calcular el numero de enteros multiplos de 2,3,5
+ */
+
+public class Reducciones {
+    public static void main(String[] args) {
+        int numHebras = 4;
+
+        //listaLong
+        long[] vector = {
+                1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,
+                16,17,18,19,20,21,22,23,24,25,26,27,28,
+                29,30,31,32,33,34,35,36,37,38,39,40, 41};
+
+
+        implementacionSecuencial(vector);
+        implementacionCiclica(vector,numHebras);
+    }
+    static void implementacionSecuencial(long[] lista){
+        System.out.println();
+        System.out.println("Implementacion secuencial");
+        //Contadores
+        int multiplosDos = 0;
+        int multiplosTres = 0;
+        int multiplosCinco = 0;
+
+        //Tiempos
+        long t1;
+        long t2;
+        double tt;
+
+        //Calculo
+        t1 = System.nanoTime();
+        for (int i = 0; i < lista.length; i++){
+            if (i % 2 == 0){
+                multiplosDos++;
+            }
+            if (i % 3 == 0){
+                multiplosTres++;
+            }
+            if (i % 5 == 0){
+                multiplosCinco++;
+            }
+        }
+        t2 = System.nanoTime();
+        tt = ((double) (t2 - t1)) / 1.0e9;
+
+        System.out.println();
+        System.out.println("Multiplos de 2: "+multiplosDos);
+        System.out.println("Multiplos de 3: "+multiplosTres);
+        System.out.println("Multiplos de 5: "+multiplosCinco);
+        System.out.println("Tiempo total: "+tt);
+    }
+
+    static void implementacionCiclica(long[] lista, int numHebras){
+        System.out.println();
+        System.out.println("Implementacion secuencial");
+
+        //Tiempos
+        long t1;
+        long t2;
+        double tt;
+
+        //Objetos
+        MiHebraCiclica[] listaHebrasCiclicas = new MiHebraCiclica[numHebras];
+        Contador contador = new Contador();
+
+        t1 = System.nanoTime();
+
+        //Start
+        for (int idHebra = 0; idHebra < numHebras; idHebra++){
+            listaHebrasCiclicas[idHebra] = new MiHebraCiclica(idHebra,numHebras,lista, contador);
+            listaHebrasCiclicas[idHebra].start();
+        }
+
+        //Join
+        for (int idHebra = 0; idHebra < numHebras; idHebra++){
+            try {
+                listaHebrasCiclicas[idHebra].join();
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+            }
+        }
+
+        t2 = System.nanoTime();
+        tt = ((double) (t2 - t1)) / 1.0e9;
+
+        //Resultado
+        System.out.println();
+        System.out.println("Multiplos de 2: "+contador.muestraMultiplosDos());
+        System.out.println("Multiplos de 3: "+contador.muestraMultiplosTres());
+        System.out.println("Multiplos de 5: "+contador.muestraMultiplosCinco());
+        System.out.println("Tiempo total: "+tt);
+    }
+}
+class MiHebraCiclica extends Thread {
+    int idHebra;
+    int numHebras;
+    long[] lista;
+    Contador contador;
+
+    public MiHebraCiclica(int idHebra, int numHebras, long[] lista, Contador contador) {
+        this.idHebra = idHebra;
+        this.numHebras = numHebras;
+        this.lista = lista;
+        this.contador = contador;
+    }
+
+    //Este es el sin reduccion, sin atomic no funciona, para el resto cambiar a atomic.
+    public void run() {
+        //Variables locales, pasar a contador
+        int multiploDos = 0;
+        int multiploTres = 0;
+        int multiploCinco = 0;
+        for (int i = idHebra; i < lista.length; i += numHebras) {
+            if (i % 2 == 0){
+                multiploDos++;
+                contador.asignaMultiplosDos(multiploDos);
+            }
+            if (i % 3 == 0){
+                multiploTres++;
+                contador.asignaMultiplosTres(multiploTres);
+            }
+            if (i % 5 == 0){
+                multiploCinco++;
+                contador.asignaMultiplosCinco(multiploCinco);
+            }
+        }
+    }
+
+}
+
+class Contador{
+    volatile int multiploDos;
+    volatile int multiploTres;
+    volatile int multiploCinco;
+
+    public void asignaMultiplosDos(int multiplos){
+        this.multiploDos = multiplos;
+    }
+    public void asignaMultiplosTres(int multiplos){
+        this.multiploTres = multiplos;
+    }
+    public void asignaMultiplosCinco(int multiplos){
+        this.multiploCinco = multiplos;
+    }
+
+    public int muestraMultiplosDos(){
+        return multiploDos;
+    }
+    public int muestraMultiplosTres(){
+        return multiploTres;
+    }
+    public int muestraMultiplosCinco(){
+        return multiploCinco;
+    }
+
+}
